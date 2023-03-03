0f289d4eb8d8ddb4547a91e49d1c05a2b4c3edd8
shakil <admin@shakil.com>
2023-03-03 23:56:45 +0600

    80% project

--- app/src/main/java/com/shakil/deposit_app/membership.java
+++ app/src/main/java/com/shakil/deposit_app/membership.java
@@ -1,14 +1,28 @@
 package com.shakil.deposit_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class membership extends AppCompatActivity {
-
+   CardView card1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_membership);
+
+        card1 = findViewById(R.id.card1);
+
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(membership.this, premium_view.class);
+                startActivity(intent);
+            }
+        });
     }
 }
